4ba60ca25464dcec29cc64d77ed76c5105694792
safcsp dev <dev@test.com>
2024-04-07 21:22:13 +0400

    Add project files.

---
+++ BackGroundCoffeeServices/BackgroundCoffeeService.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Threading;
+
+public class BackgroundCoffeeService : BackgroundService
+{
+    readonly ILogger<BackgroundCoffeeService> _logger;
+
+    public BackgroundCoffeeService(ILogger<BackgroundCoffeeService> logger)
+    {
+        _logger = logger;
+    }
+    
+
+    protected  async override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("Coffee making at: {time}", DateTimeOffset.Now);
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
---
+++ BackGroundCoffeeServices/Controllers/CoffeeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackGroundCoffeeServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoffeeController : ControllerBase
+    {
+        [HttpGet("OrderCoffee")]
+        public string Get()
+        {
+            return "Coffee is ready";
+        }
+    }
+}
